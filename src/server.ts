602df6f84daaf9aa237a21bccd4d1909fa8f3f03
luiscaputo <luiscaputo15@gmail.com>
2021-06-23 10:23:47 +0100

    :zap: configure-database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
+import "typeorm";
 import express from "express"
 
 const app = express();
 
-    app.get('/', (req, res) => {
-        return res.send('Estou rodando')
-    })
 
 app.listen(8080, () => console.log('Server is Running'))
 
